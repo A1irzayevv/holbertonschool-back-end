1c8f4e27bef76eca03828482fd087b116379ab79
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-22 17:36:30 +0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,7 +4,6 @@ first rest api code
 """
 import requests
 import sys
-import csv
 
 
 def get_todo_rest_api(id):
@@ -25,20 +24,10 @@ def get_todo_rest_api(id):
     csv_filename = f"{id}.csv"
 
     with open(csv_filename, 'w', newline='') as csvfile:
-        fieldnames = ['USER_ID',
-                      'USERNAME',
-                      'TASK_COMPLETED_STATUS',
-                      'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
         for todo in todos_data:
             task_completed = 'True' if todo['completed'] else 'False'
-            writer.writerow({
-                'USER_ID': id,
-                'USERNAME': name,
-                'TASK_COMPLETED_STATUS': task_completed,
-                'TASK_TITLE': todo['title']
-                })
+            title = todo['title']
+            csvfile.write(f'"{id}","{name}","{task_completed}","{title}"\n')
 
 
 if __name__ == "__main__":
