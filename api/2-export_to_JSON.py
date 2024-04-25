866c976115fc760c7202764d60f3b7b0eb60a704
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-25 18:20:25 +0400

    2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+task 2
+"""
+import requests
+import sys
+import json
+
+
+def save_to_json(id):
+    """save func"""
+    url = "https://jsonplaceholder.typicode.com"
+
+    user_url = f"{url}/users/{id}"
+
+    response = requests.get(user_url)
+    user_data = response.json()
+    name = user_data['username']
+
+    todos_url = f"{url}/todos?userId={id}"
+
+    response = requests.get(todos_url)
+    todos_data = response.json()
+
+    json_data = {
+            str(id): [
+                {
+                    "task": todo["title"],
+                    "completed": todo["completed"],
+                    "username": name
+                }
+                for todo in todos_data
+            ]
+        }
+
+    json_filename = f"{id}.json"
+
+    with open(json_filename, "w") as jsonfile:
+        json.dump(json_data, jsonfile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        id = sys.argv[1]
+        save_to_json(id)
